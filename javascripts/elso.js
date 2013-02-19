f4aab0344a1d4fe82e4f87cc8f6c779fef401619
Elabro <elabrobyte@gmail.com>
2013-02-19 23:42:10 +0100

    Update javascripts/elso.js

--- javascripts/elso.js
+++ javascripts/elso.js
@@ -1,16 +1,18 @@
 var msginput;
 var gomb;
 var outputContainer;
-
+var msg;
 function onClick() {
         alert( msginput.value.length + ' szóból áll a szöveg!');
 }
 
 function setUp() {
+	msg = document.getElementById('msg');
 	msginput = document.getElementById('msginput');
         gomb = document.getElementById('gomb');
 	outputContainer = document.getElementById('output');
 	gomb.addEventListener('click', onClick);
+	msg.value = msginput.value.length;
 }
 
 document.addEventListener('DOMContentLoaded', setUp);

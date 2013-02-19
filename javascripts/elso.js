b589724f1ca7c6ee443304ceda938f8d9dacc1a7
Elabro <elabrobyte@gmail.com>
2013-02-19 22:52:10 +0100

    Update javascripts/elso.js

--- javascripts/elso.js
+++ javascripts/elso.js
@@ -4,11 +4,11 @@ function ccounter_func(msginputId,ccounter_inputId)
         var ccounter_input = document.getElementById(ccounter_inputId);
         var msglen = msginput.value.length;
  
-        ccounter_input.value = (msglen > 0) ? max - msglen : max;
+        ccounter_input.value = msglen;
  
-        if(msglen > max) {
-                alert("Üzenet max hossza: "+max);
-                msginput.value = msginput.value.substring(0,max);
+        if(msglen > 0 ) {
+                alert("Üzenet max hossza: "+msglen);
+                msginput.value = msginput.value.substring(0,msglen);
                 ccounter_input.value=0;
         }
         

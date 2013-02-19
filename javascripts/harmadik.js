b0fcf53adc1c12b0ed610df9582ef4a33a329fbf
Elabro <elabrobyte@gmail.com>
2013-02-19 15:00:50 -0800

    Create harmadik.js

---
+++ javascripts/harmadik.js
@@ -0,0 +1,25 @@
+var myHandler = (function() {
+  "use strict";
+ var msginput, gomb, outputContainer,  msg;
+
+function onClick() {
+        alert( msginput.value.length + ' szóból áll a szöveg!');
+}
+
+function onKeyUp(){
+	msg.value = msginput.value.length;
+
+}
+
+function setUp() {
+	msg = document.getElementById('msg');
+	msginput = document.getElementById('msginput');
+        gomb = document.getElementById('gomb');
+	outputContainer = document.getElementById('output');
+	gomb.addEventListener('click', onClick);
+	msginput.addEventListener('keyup', onKeyUp);
+}
+return setUp;
+})();
+
+document.addEventListener('DOMContentLoaded', setUp);

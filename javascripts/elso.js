6d33aa89f56ea8993c84d0d370ff8675634cb307
Elabro <elabrobyte@gmail.com>
2013-02-19 23:07:40 +0100

    Update javascripts/elso.js

--- javascripts/elso.js
+++ javascripts/elso.js
@@ -1,17 +1,14 @@
-
 function counter_func(msginputId,ccounter_inputId)
 {	
 	var gomb;
 	var outputContainer;
-        var msginput = document.getElementById(msginputId);
-        var counter_input = document.getElementById(counter_inputId);
         var msglen = msginput.value.length;
  
         counter_input.value = msglen;
  
 }      
-function onClick(ev) {
-        createNewOutput( ++msglen + ' szóból áll a szöveg!');
+function onClick() {
+        alert( ++msglen + ' szóból áll a szöveg!');
 }
         
 function setUp() {

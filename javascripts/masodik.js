d21b538bf4654baf9e981ac3aacb8d20818a8b73
Elabro <elabrobyte@gmail.com>
2013-02-19 14:51:55 -0800

    Create masodik.js

---
+++ javascripts/masodik.js
@@ -0,0 +1,26 @@
+var myHandler = {
+ msginput: null,
+ gomb: null,
+ outputContainer: null,
+ msg: null,
+
+onClick: function() {
+        alert( msginput.value.length + ' szóból áll a szöveg!');
+},
+
+onKeyUp: function (){
+  msg.value = msginput.value.length;
+
+},
+
+setUp: function () {
+	myHandler.msg = document.getElementById('msg');
+	myHandler.msginput = document.getElementById('msginput');
+        	myHandler.gomb = document.getElementById('gomb');
+	myHandler.outputContainer = document.getElementById('output');
+	myHandler.gomb.addEventListener('click', onClick);
+	myHandler.msginput.addEventListener('keyup', onKeyUp);
+
+}
+}
+document.addEventListener('DOMContentLoaded', setUp);
